c67105b9de9460cba6af6eb261fe2509e21f96f4
IlyaGuretsky <borec023017@gmail.com>
2019-11-29 00:49:20 +0300

    Add test controller

--- billboard_advertising/rest/src/main/java/com/guretsky_tsarionok/controller/UserController.java
+++ billboard_advertising/rest/src/main/java/com/guretsky_tsarionok/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.guretsky_tsarionok.controller;
 
+import com.guretsky_tsarionok.model.User;
+import com.guretsky_tsarionok.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/u")
 public class UserController {
+    @Autowired
+    UserService service;
+
+    @GetMapping
+    public List<User> getUser() {
+        return service.getAll();
+    }
 }
