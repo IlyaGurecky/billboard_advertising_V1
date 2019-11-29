c802dd48f700d34cc13be2fa4f366acb901e15b7
IlyaGuretsky <borec023017@gmail.com>
2019-11-29 22:13:10 +0300

    Update user repository

--- billboard_advertising/core/src/main/java/com/guretsky_tsarionok/repository/UserRepository.java
+++ billboard_advertising/core/src/main/java/com/guretsky_tsarionok/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.guretsky_tsarionok.repository;
 import com.guretsky_tsarionok.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
