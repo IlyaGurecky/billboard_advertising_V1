67327f1e460de88aac027861967b6ec51bc61759
IlyaGuretsky <borec023017@gmail.com>
2019-11-30 10:52:12 +0300

    Add dto

---
+++ billboard_advertising/model/src/main/java/com/guretsky_tsarionok/dto/AdvertisingDto.java
@@ -0,0 +1,18 @@
+package com.guretsky_tsarionok.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AdvertisingDto {
+    String contentPath;
+    String name;
+}
---
+++ billboard_advertising/model/src/main/java/com/guretsky_tsarionok/dto/ScheduleDto.java
@@ -0,0 +1,21 @@
+package com.guretsky_tsarionok.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ScheduleDto {
+    String name;
+    int frequency;
+    List<Long> advIds;
+}
